c69c839d28af9fd1d6e560652f3e9145c93b89a7
diyabhimani <diyabhimani@gmail.com>
2026-02-05 16:03:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+echo "<li><a href='diya/categorydelete.php'>Category Delete</a></li>";
+echo "<li><a href='diya/categoryinsert.php'>Category insert</a></li>";
+echo "<li><a href='diya/categoryview.php'>Category view</a></li>";
+echo "<li><a href='diya/categoryupdate.php'>Category update</a></li>";
+echo "<li><a href='diya/userdelete.php'>User Delete</a></li>";
+echo "<li><a href='diya/userinsert.php'>User insert</a></li>";
+echo "<li><a href='diya/userupdate.php'>User update</a></li>";
+echo "<li><a href='diya/userview.php'>User view</a></li>";
+echo "<li><a href='diya/trackerdelete.php'>tracker delete</a></li>";
+echo "<li><a href='diya/trackerinsert.php'>traker insert</a></li>";
+echo "<li><a href='diya/trackerupdate.php'>tacker  update</a></li>";
+echo "<li><a href='diya/trackerview.php'>tracker view</a></li>";
+echo "<li><a href='diya/signup.php'>signup</a></li>";
+echo "<li><a href='diya/signin.php'>signin</a></li>";
+
+
